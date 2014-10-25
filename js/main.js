90820e5fd5a627c517b570f65cf27b359e1248fa
Prosenjit Manna <prosenjit-web@users.noreply.github.com>
2014-10-25 22:36:02 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ jQuery('.ie7 body, .ie8 body').prepend('<p class="chromeframe">You are using an 
 //remove dreamwever class from body
 jQuery('.drw').removeClass('drw');
 
-var apiUrl ="http://web-c7082037-76b4-4925-a2f1-eeada0733ace.runnable.com/"
+var apiUrl ="http://web-b08d9ee6-ffaf-4914-8cac-3b819ab849e1.runnable.com/"
 
 if (!url('?')) {
 	var tmp_header = jQuery("#tmp_header");
